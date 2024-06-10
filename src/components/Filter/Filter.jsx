0053e5f4f2928196f492335b1641acc27e6220a8
GibsoN92 <52374403+GibsoN92@users.noreply.github.com>
2024-06-10 19:39:18 +0200

    v1

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import css from "./Filter.module.scss";
+import PropTypes from "prop-types";
+
+const Filter = ({ changeHandler }) => {
+  return (
+    <div className={css["container"]}>
+      <span>Find contacts by name</span>
+      <input
+        type="text"
+        name="filter"
+        onChange={changeHandler}
+        className={css["filter-input"]}></input>
+    </div>
+  );
+};
+
+Filter.propTypes = {
+  changeHandler: PropTypes.func.isRequired,
+};
+
+export default Filter;
