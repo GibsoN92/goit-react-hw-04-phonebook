e2b5a161a2a13d9b28633d5359c9f272e99ea383
GibsoN92 <52374403+GibsoN92@users.noreply.github.com>
2024-06-14 18:33:55 +0200

    v2

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -33,7 +33,7 @@ class ContactForm extends Component {
         <input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[a-zA-Z '\-]+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
@@ -43,7 +43,7 @@ class ContactForm extends Component {
         <input
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^\+?[0-9\(\) \-]+$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
